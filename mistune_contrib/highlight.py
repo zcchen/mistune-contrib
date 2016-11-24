2cabbba4457e137dea31d1a07dffb4f30bc6abdf
zcchen <chen.bill.bill@gmail.com>
2016-11-24 10:31:58 +0800

    Fixed a bug which would be caused when 'inlinestyles=None' or 'linenos=None'.

--- mistune_contrib/highlight.py
+++ mistune_contrib/highlight.py
@@ -38,6 +38,6 @@ def block_code(text, lang, inlinestyles=False, linenos=False):
 class HighlightMixin(object):
     def block_code(self, text, lang):
         # renderer has an options
-        inlinestyles = self.options.get('inlinestyles')
-        linenos = self.options.get('linenos')
+        inlinestyles = True if self.options.get('inlinestyles') else False
+        linenos = True if self.options.get('linenos') else False
         return block_code(text, lang, inlinestyles, linenos)
